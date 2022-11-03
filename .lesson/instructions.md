e70dfbd6dcf51c99450829128c70d18c1c5e0326
David M. Wilson <davidmichaelwilson81@gmail.com>
2022-11-03 14:22:13 -0500

    added from replit

---
+++ .lesson/instructions.md
@@ -0,0 +1,7 @@
+# Instructions  
+
+1. Create a variable called bonusPoints.
+2. Initialize it to 50.
+3. Increase it to 100.
+4. Decrease it down to 25.
+5. Finally increase it to 70.
---
+++ script.js
@@ -0,0 +1,30 @@
+// document.getElementById("count").innerText = 5
+
+// let count = 5
+// count = 20
+// count = count + 1
+// count = count + 5
+// count = count - 2
+// count = count / 2
+
+// console.log(count)
+
+
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50 
+// bonusPoints = bonusPoints - 75
+// bonusPoints = bonusPoints + 45
+
+// console.log(bonusPoints)
+
+function increment() {
+  console.log("I clicked the button!")
+}
+
+
+
+let count = 0
+
+
+
+console.log(count)
